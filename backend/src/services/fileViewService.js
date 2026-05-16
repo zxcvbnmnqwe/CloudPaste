cd29bfb66a80bcf7903a8521464caca72f41ce0c
zxcvbnmnqwe <156883113+zxcvbnmnqwe@users.noreply.github.com>
2026-05-16 21:59:39 +0800

    asd

--- backend/src/services/fileViewService.js
+++ backend/src/services/fileViewService.js
@@ -227,10 +227,10 @@ for (const chunk of chunks) {
         );
 
            // 设置CORS头部
-        response.headers.set("Access-Control-Allow-Origin", "*");
-        response.headers.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS");
-        response.headers.set("Access-Control-Allow-Headers", "Range, Content-Type");
-        response.headers.set("Access-Control-Expose-Headers", "siyou, Content-Length, Content-Range, Accept-Ranges");
+        // response.headers.set("Access-Control-Allow-Origin", "*");
+        // response.headers.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS");
+        // response.headers.set("Access-Control-Allow-Headers", "Range, Content-Type");
+        // response.headers.set("Access-Control-Expose-Headers", "siyou, Content-Length, Content-Range, Accept-Ranges");
          // 禁用压缩
         response.headers.set('Content-Encoding', 'identity'); 
         // 更新响应头
@@ -239,10 +239,10 @@ for (const chunk of chunks) {
         //文件大小
         if (fileRecord && fileRecord.size != null && typeof fileRecord.size === 'number') {
              response.headers.set("Content-Length", fileRecord.size);
-             response.headers.set("siyou", fileRecord.size);
+             response.headers.set("X-File-Size", fileRecord.size);
         }else{
             response.headers.set("Content-Length", "100000000");
-            response.headers.set("siyou", "100000000");
+            response.headers.set("X-File-Size", "100000000");
         }
          // ↓↓↓↓↓ 【关键：强制开启 Cloudflare 缓存】↓↓↓↓↓
           response.headers.set("Cache-Control", "public, max-age=604800, s-maxage=604800");
